256837a012d5c38062fbce9a880ccc8979c3b76f
yuriinahrebnyi <yurii.nahrebnyi.ki.2020@lpnu.ua>
2022-10-10 16:36:24 +0300

    Lab1.0

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        String message;
+        message = "\nYura Nahrebnyi KI-36 lab1";
+        System.out.println(message);
+    }
+}
